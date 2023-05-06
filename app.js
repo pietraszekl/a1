a4834d115149dd54ef168a93d62d38808446d105
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2023-05-06 17:37:38 +0200

    a1 - logic for submenu

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 function initMainMenu() {
   const topLevelNav = document.getElementsByClassName("js-topLevelNav");
+  const subLevelNav = document.getElementsByClassName("js-subLevelNav");
   Array.from(topLevelNav).forEach((navEl) => new TopMenuItem(navEl, topLevelNav));
+  Array.from(subLevelNav).forEach((subNavEl) => new SubMenuItem(subNavEl));
 }
 
-class TopMenuItem {
-  constructor(el, allElements) {
+class MenuItem {
+  constructor(el, allElements = null) {
     this.el = el;
     this.allElements = allElements;
     this.el.addEventListener("click", this);
   }
 
+  handleEvent(event) {
+    event.stopPropagation();
+    console.log("event", event);
+  }
+}
+
+class SubMenuItem extends MenuItem {
+  constructor(...args) {
+    super(...args);
+  }
+
+  handleEvent() {
+    alert(event.target.innerText);
+  }
+}
+
+class TopMenuItem extends MenuItem {
+  constructor(...args) {
+    super(...args);
+  }
+
   ACTIVE_CLASS = "is-active";
 
   handleEvent(event) {
+    event.stopPropagation();
     this.toggleActiveClass(event);
   }
 
